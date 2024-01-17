6e3d69d751973ce8405735ab3c28726c3f867d66
lgalvan-alg <lgalvan@ifp-alguero.org>
2024-01-17 20:47:56 +0100

    porfolio bueno

---
+++ wordpress/wp-content/themes/portafoliomio/index.php
@@ -0,0 +1,128 @@
+
+
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8" http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <title>PORFOLIO</title>
+    <link rel=StyleSheet href="estilo.css" type="text/css">
+
+    <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'template_url' ); ?>" />
+
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'template_url' ); ?>/css/style.css" />
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    <script src="slider/sss.min.js"></script><link rel="stylesheet" href="slider/sss.css" type="text/css" media="all">
+    <script>
+      jQuery(function($) {
+        $('.slider').sss({
+        slideShow : true, // Set to false to prevent SSS from automatically animating.startOn : 0, // Slide to display first. Uses array notation (0 = first slide).transition : 400, // Length (in milliseconds) of the fade transition.speed : 3500, // Slideshow speed in milliseconds.showNav : true // Set to false to hide navigation arrows.
+        });
+
+      });
+
+    </script>
+    <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
+    <script>
+    $(document).ready(function(){
+        $('.block h2').on('click',function(){
+            $(this).parent().find('ul').slideToggle('slow');
+        });
+    });
+    </script>
+
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&family=Playfair+Display+SC:ital,wght@0,400;0,700;0,900;1,400;1,700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+  <div id="cabecera">
+
+              <img src="img/logotipo2.jpg" width="600px" height="280px">
+
+  </div>
+
+
+
+
+
+
+    <nav class="normal">
+
+        <ul>
+          <li><a href="#seccionuno">SECCIÓN 1</a></li>
+          <li><a href="#secciondos">SECCIÓN 2</a></li>
+          <li><a href="#secciontres">SECCIÓN 3</a></li>
+
+          <div class="clear"></div>
+        </ul>
+
+    </nav>
+
+
+<div id="contenedorinicio">
+
+        <div class="slider">
+          <img src="img/imgslider1.jpg"  width="400px"/>
+          <img src="img/imgslider2.jpg"  width="400px"/>
+          <img src="img/imgslider3.jpg"  width="400px"/>
+      </div>
+
+    <div id="lineadivisoria"></div>
+
+
+    <div id="quienes">
+      <p>PORFOLIO 2023</p>
+    </div>
+
+    
+    <div id="divisoriados"></div>
+
+    <div class="clear"></div>
+<div class="contenedoracts">
+  <h1 id="seccionuno">SECCIÓN 1</h1>
+  <div class="actividades" id="actuno"><h1><a href="mp08_UF1_01_galvanl/index.html"  target="_blank" blank>HELLO WORLD</a></h1> </div>
+  <div class="actividades" id="actdos"><h1><a href="mp08_UF1_02_horari_lgalvan/index.html" target="_blank">HORARIO</a></h1> </div>
+  <div class="actividades" id="acttres"><h1><a href="mp08_UF1_03_feedly_galvanl/index.html" target="_blank">FEEDLY</a></h1> </div>
+  <div class="actividades" id="actquatro"><h1><a href="mp08_UF1_04_ifpagram_galvanl/index.html" target="_blank">IFPAGRAM</a></h1> </div>
+</div>
+
+<div class="contenedoracts">
+  <h1 id="secciondos">SECCIÓN 2</h1>
+  <div class="actividades" id="actcinco"><h1><a href="mp08_UF1_05_escacs_galvanl/index.html" target="_blank">ESCACS</a></h1> </div>
+  <div class="actividades" id="actseis"><h1><a href="mp08_UF1_06_absolute_galvanl/index.html" target="_blank">ABSOLUTE</a></h1> </div>
+  <div class="actividades" id="actsiete"><h1><a href="mp08_UF1_07_floreta_galvanl/index.html" target="_blank">FLORETA</a></h1> </div>
+  <div class="actividades" id="actocho"><h1><a href="mp08_UF1_08_cara_galvanl/index.html" target="_blank">CARA</a></h1> </div>
+</div>
+
+<div class="contenedoracts">
+  <h1 id="secciontres">SECCIÓN 3</h1>
+  <div class="actividades" id="actnueve"><h1><a href="mp08_UF1_09_photoshop_lgalvan/index.html" target="_blank">PHOTOSHOP</a></h1> </div>
+  <div class="actividades" id="actdiez"><h1><a href="mp08_UF1_10_examen_galvanl/index.html" target="_blank">EXAMEN 2.0</a></h1> </div>
+  <div class="actividades" id="actexamen"><h1><a href="mp08_UF1_10_examen_galvanl/index.html" target="_blank">E1 PEL.LYS</a></h1> </div>
+  <div class="actividades" id="actenproceso"><h1><a href="mp08_UF1_11_porfolio_galvanl/index.html" target="_blank">...</a></h1> </div>
+</div>
+
+
+
+
+
+  </div>
+
+
+
+
+
+
+<div class="clear"></div>
+
+ <div id="pie">
+
+<h1>© LORENA GALVAN CASADO DE2</h1>
+</div>
+
+  </body>
+
+
+</html>
