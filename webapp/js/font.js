e15d5fc8bbf118bcca95dc55d3e5250134226b2b
lgalvan-alg <lgalvan@ifp-alguero.org>
2024-02-06 20:51:14 +0100

    cambios

---
+++ webapp/js/font.js
@@ -0,0 +1,24 @@
+
+var fs = 1;
+
+function petit(){
+    fs = fs-0.5;
+    document.getElementByClassName("text").style.fontSize=(fs+"rem");
+   // alert(fs+"rem");
+}
+    
+function mitja(){
+    fs = 1.5;
+    document.getElementByClassName("text").style.fontSize=(fs+"rem");
+   // alert(fs+"rem");
+}
+
+function gran(){
+    fs = fs+0.5;
+    document.getElementByClassName("text").style.fontSize=(fs+"rem");
+   // alert(fs+"rem");
+}
+ 
+document.getElementById("petit").addEventListener("click", petit);
+document.getElementById("mitja").addEventListener("click", mitja);
+document.getElementById("gran").addEventListener("click", gran);
